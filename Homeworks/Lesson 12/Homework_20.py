c258ce0b2ce6eb08985334e78d14ed6dc1eb49bd
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-04-10 17:45:00 +0300

    Add files via upload

---
+++ Homeworks/Lesson 12/Homework_20.py
@@ -0,0 +1,69 @@
+import math
+
+
+class Point(object):
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def distance_from_origin(self):
+        return math.hypot(self.x, self.y)
+
+
+class Circle(Point):
+
+    def __init__(self, radius, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __str__(self):
+        return super().__str__()[:-1] + f', radius={self.radius})'
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        radius = self.radius + other.radius
+        return Circle(radius, x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        radius = abs(self.radius - other.radius)
+        if radius:
+            return Circle(radius, x, y)
+        else:
+            return Point(x, y)
+    
+    def edge_distance_from_origin(self):
+        return abs(self.distance_from_origin() - self.radius)
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        return math.pi * (self.radius**2)
+
+
+first_circle = Circle(5, 3, 4)
+second_circle = Circle(10, 4, 1)
+
+print(first_circle - second_circle)
+second_circle = Circle(5, 4, 1)
+print(first_circle - second_circle)
+
+
