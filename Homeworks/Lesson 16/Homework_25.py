ae1dd93bd195ffb6aa60f0851538c0e6a7970a35
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2023-05-15 03:42:25 +0300

    Add files via upload

---
+++ Homeworks/Lesson 16/Homework_25.py
@@ -0,0 +1,67 @@
+MyExit = ("выход","exit", "quit", "e", "q")
+
+
+def string_checker(my_input):
+    initial_value = my_input
+    negative_flag = False
+    invalid_value = False
+    fractional_flag = False
+    zero_flag = False
+
+    my_input = my_input.replace(',', '.')
+
+    if my_input.rfind('+') > 0:
+        invalid_value = True
+    elif my_input.rfind('+') == 0:
+        my_input = my_input.replace('+', '')
+
+    if not invalid_value:
+        if my_input.rfind('-') > 0:
+            invalid_value = True
+        elif my_input.rfind('-') == 0:
+            my_input = my_input.replace('-', '')
+            negative_flag = True
+
+    if not invalid_value:
+        if my_input.count('.') > 1:
+            invalid_value = True
+        elif my_input.count('.') == 1:
+            fractional_flag = True
+            if my_input.rfind('.') == 0:
+                my_input = f'0{my_input}'
+            if not my_input.replace('.', '').isdigit():
+                invalid_value = True
+
+    negative = "отрицательное" if negative_flag else "положительное"
+    Negative = "дробное" if fractional_flag else "целое"
+
+    if not invalid_value:
+        if fractional_flag:
+            number = float(my_input)
+        else:
+            number = int(my_input)
+
+    if not invalid_value and negative_flag:
+            number = number * -1
+
+    if not invalid_value and number == 0:
+        zero_flag = True
+
+    if invalid_value:
+        output = f'Вы ввели не корректное число: {initial_value}'
+    elif zero_flag:
+        output = 'Вы ввели ноль'
+    else:
+        output = f'Вы ввели {negative} {Negative} число: {number}'
+
+    return output
+
+
+while True:
+    read = input("Введите число либо команду выходаn\n")
+    read = read.lower()
+
+    if read in MyExit:
+        break
+
+    print(string_checker(read))
